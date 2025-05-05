b706ed00fee6a75670faed4af6b2f67737ede1b1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-05-05 23:39:03 +0200

    step 13a - handle incoming WS messages in the frontend

---
+++ static/js/clientside-rendering.js
@@ -0,0 +1,11 @@
+function createNote(note) {
+    console.log("Creating note:", note);
+}
+
+function updateNote(note) {
+    console.log("Updating note:", note);
+}
+
+function deleteNote(noteId) {
+    console.log("Deleting note with ID:", noteId);
+}
