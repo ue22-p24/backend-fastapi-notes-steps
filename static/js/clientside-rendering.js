21e0a45ab3993a9713204aa919e85349b0974b10
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-05-05 23:39:04 +0200

    step 13c - CSR on note creation

--- static/js/clientside-rendering.js
+++ static/js/clientside-rendering.js
@@ -1,5 +1,19 @@
-function createNote(note) {
-    console.log("Creating note:", note);
+function createNoteElement(note) {
+    const elt = document.createElement("li");
+    elt.classList.add("note");
+    elt.id = `note-${note.id}`;
+    const html = `
+    <div class="title">${note.title}
+        <button class="delete-button" onclick="note_delete(this, ${note.id})">🗑</button>
+    </div>
+    <div class="line">
+        <span>${note.description}</span>
+        <input type="checkbox" ${note.done ? "checked" : ""}
+        onchange="note_done_changed(this, ${note.id})" />
+    </div>
+`
+    elt.innerHTML = html;
+    document.querySelector("ul.notes").appendChild(elt);
 }
 
 function updateNote(note) {
